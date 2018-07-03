326359f588be85004da2f311d19a152c8d1f9560
marijnschilling <marijnschilling@gmail.com>
2018-07-03 16:58:51 +0200

    Don't use viewcontrollers but use views

--- MyPlayground.playground/Pages/Sizes.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Sizes.xcplaygroundpage/Contents.swift
@@ -2,16 +2,16 @@ import UIKit
 import PlaygroundSupport
 import TinyConstraints
 //:# Sizes
-//: First we set up our ViewController and the UIView that we want to add the constraints to.
-let viewController = UIViewController()
+//: First we set up our background view and the view that we want to add the constraints to.
+let backgroundView = UIView(frame: CGRect(x: 0, y: 0, width: 400, height: 400))
+PlaygroundPage.current.liveView = backgroundView
 let constraintView = UIView()
-viewController.view.backgroundColor = TinyTheme.backgroundColor
+backgroundView.backgroundColor = TinyTheme.backgroundColor
 constraintView.backgroundColor = TinyTheme.constraintViewColor
 /*:
 > Always remember to add the view as a subview before you add constraints, otherwise your app will crash 💥
 */
-viewController.view.addSubview(constraintView)
-
+backgroundView.addSubview(constraintView)
 /*:
 Setting width and height with NSLayoutConstraint:
 
@@ -25,6 +25,4 @@ constraintView.height(100)
 constraintView.width(100)
 //: or even shorter, both at once:
 constraintView.size(CGSize(width: 100, height: 100))
-
-PlaygroundPage.current.liveView = viewController
 //: [Edges](@next)
